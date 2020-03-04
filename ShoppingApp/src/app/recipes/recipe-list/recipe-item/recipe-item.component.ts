69c447838f8c578a7b2a947c6709493315b033e0
richard <richosojason@msn.com>
2020-03-04 10:27:05 -0500

    fixed recipe-item.component

--- ShoppingApp/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ ShoppingApp/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+
 import { Recipe } from "../../recipe.model";
 
 @Component({
@@ -12,7 +13,7 @@ export class RecipeItemComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 
   onSelected() {
     this.recipeSelected.emit();
