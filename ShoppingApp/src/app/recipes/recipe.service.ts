424455d4cd5345094d77aee2384fc955bbbfcde5
richard <richosojason@msn.com>
2020-03-04 11:19:16 -0500

    added ingredients to recipe-detail.component

--- ShoppingApp/src/app/recipes/recipe.service.ts
+++ ShoppingApp/src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       "A Test Recipe",
       "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("meat", 1), new Ingredient("french fries", 20)]
     ),
     new Recipe(
       "Another Test Recipe",
       "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("Buns", 2), new Ingredient("meat", 1)]
     )
   ];
 
