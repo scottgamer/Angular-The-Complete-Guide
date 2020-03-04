b6cdea517feda7c450b2bdd162f3aa43c8d69b7f
richard <richosojason@msn.com>
2020-03-04 16:45:14 -0500

    added recipe-start.component

--- ShoppingApp/src/app/app.module.ts
+++ ShoppingApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DropdownDirective } from "./shared/dropdown.directive";
 
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
 import { AppRoutingModule } from "./app-routing.module";
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { AppRoutingModule } from "./app-routing.module";
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipeStartComponent
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ShoppingListService],
