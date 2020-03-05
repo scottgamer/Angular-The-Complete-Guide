38d8e501b60757c48f7f010312edb57373c33dbd
richard <richosojason@msn.com>
2020-03-05 17:32:39 -0500

    fixed typo in template

--- ShoppingApp/src/app/shopping-list/shopping-list.component.ts
+++ ShoppingApp/src/app/shopping-list/shopping-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "./shopping-list.service";
-import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-shopping-list",
