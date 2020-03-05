6df464b0b5820e1a872c7b5923e535ce43e23b40
richard <richosojason@msn.com>
2020-03-05 17:31:23 -0500

    added validation to form

--- ShoppingApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ ShoppingApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, OnDestroy, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Subscription } from "rxjs";
 
 import { Ingredient } from "../../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list.service";
-import { NgForm } from "@angular/forms";
-import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-shopping-edit",
@@ -11,7 +11,7 @@ import { Subscription } from "rxjs";
   styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
-  @ViewChild("f") slForm: NgForm;
+  @ViewChild("f", { static: false }) slForm: NgForm;
   subscription: Subscription;
   editMode = false;
   editedItemIndex: number;
