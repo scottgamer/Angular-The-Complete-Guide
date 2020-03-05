d88ef52c2f894cbbe27619b4b0fc922a166ed84e
richard <richosojason@msn.com>
2020-03-05 17:48:39 -0500

    fixed auth.component

--- ShoppingApp/src/app/auth/auth.component.ts
+++ ShoppingApp/src/app/auth/auth.component.ts
@@ -1,61 +1,50 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthService, AuthResponseData } from "./auth.service";
 import { Observable } from "rxjs";
+import { AuthService, AuthResponseData } from "./auth.service";
 import { Router } from "@angular/router";
 
 @Component({
   selector: "app-auth",
-  templateUrl: "./auth.component.html",
-  styleUrls: ["./auth.component.css"],
-  providers: [AuthService]
+  templateUrl: "./auth.component.html"
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
 
   constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
-
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
     const value = form.value;
     const { email, password } = value;
 
-    let authObservable: Observable<AuthResponseData>;
+    let authObs: Observable<AuthResponseData>;
 
     this.isLoading = true;
+
     if (this.isLoginMode) {
-      authObservable = this.authService.login(email, password);
+      authObs = this.authService.login(email, password);
     } else {
-      authObservable = this.authService.signUp(email, password);
+      authObs = this.authService.signUp(email, password);
     }
 
-    authObservable.subscribe(
+    authObs.subscribe(
       resData => {
         console.log(resData);
         this.isLoading = false;
         this.router.navigate(["/recipes"]);
       },
-      errorRes => {
-        switch (errorRes.error.error.message) {
-          case "EMAIL_EXISTS":
-            this.error = "This email exists already";
-            break;
-          case "EMAIL_NOT_FOUND":
-            this.error = "This email doesn't exist";
-            break;
-          case "INVALID_PASSWORD":
-            this.error = "This password is not correct";
-            break;
-          default:
-            this.error = "An unexpected error ocurred";
-        }
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
         this.isLoading = false;
       }
     );
