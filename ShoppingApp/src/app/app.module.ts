7444657fd2cfb5fec73915423c78cf5b5219cc98
richard <richosojason@msn.com>
2020-03-05 15:03:21 -0500

    added signup request

--- ShoppingApp/src/app/app.module.ts
+++ ShoppingApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -13,10 +15,10 @@ import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-ed
 import { DropdownDirective } from "./shared/dropdown.directive";
 
 import { ShoppingListService } from "./shopping-list/shopping-list.service";
-import { AppRoutingModule } from "./app-routing.module";
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { AuthComponent } from './auth/auth.component';
+import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { AuthComponent } from "./auth/auth.component";
+import { LoadingSpinnerComponent } from "./shared/loading-spinner/loading-spinner.component";
 
 @NgModule({
   declarations: [
@@ -31,9 +33,10 @@ import { AuthComponent } from './auth/auth.component';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
-    AuthComponent
+    AuthComponent,
+    LoadingSpinnerComponent
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
--- ShoppingApp/src/app/auth/auth.component.ts
+++ ShoppingApp/src/app/auth/auth.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
-  styleUrls: ["./auth.component.css"]
+  styleUrls: ["./auth.component.css"],
+  providers: [AuthService]
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 
@@ -19,6 +23,30 @@ export class AuthComponent implements OnInit {
 
   onSubmit(form: NgForm) {
     const value = form.value;
+    const { email, password } = value;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      ///
+    } else {
+      this.authService.signUp(email, password).subscribe(
+        resData => {
+          console.log(resData);
+          this.isLoading = false;
+        },
+        errorRes => {
+          switch (errorRes.error.error.message) {
+            case "EMAIL_EXISTS":
+              this.error = "This email exists already";
+              break;
+            default:
+              this.error = "An unexpected error ocurred";
+          }
+          this.isLoading = false;
+        }
+      );
+    }
+
     form.reset();
   }
 }
